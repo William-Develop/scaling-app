7b2c40aa2b0297b40534f38dfdfef1dfe6cae248
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-04 15:10:32 -0600

    add statusMonitor express middleware

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const helmet = require("helmet"); 
 const http = require("http"); // Allows to create an HTTP server.
+const statusMonitor = require('express-status-monitor');
 
 const app = express();
+
+app.use(statusMonitor());
+
 app.use(helmet()); // Secure Express apps with various HTTP headers
 
 app.get("/",(req, res) => {
     let i = 0;
     while(i < 1e7) i++;
-    res.send(`✨ Hello from worker number: ${process.pid} ✨\n This is a Scaling demo!`)
+    res.send(`✨ Hello from worker number: ${process.pid} ✨\n This is a demo!`)
 });
 
 // Exports function that starts the server.
